26d6ca9b772b87e2b82f73f1913cda48a20b1104
LiamVDB <Liam.VandenBroeck@ugent.be>
2024-03-18 23:41:32 +0100

    Eerste werk aan het project. -FXML Opgezet -Controller klasse Opgezet -BasisPanes bepaald -CSS bestand aangemaakt. -Eerste werk aan Linkse AnchorPane

--- src/main/java/be/ugent/objprog/ugentopoly/Ugentopoly.java
+++ src/main/java/be/ugent/objprog/ugentopoly/Ugentopoly.java
@@ -2,32 +2,32 @@ package be.ugent.objprog.ugentopoly;
 
 import be.ugent.objprog.dice.Dice;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Ugentopoly extends Application {
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException{
         final Dice dice = new Dice();
 
         Button btn = new Button();
         btn.setText("Rol dobbelstenen");
         btn.setOnAction(event -> dice.roll(t -> System.out.println("Klaar met rollen")));
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 300, 250);
-
-        stage.setTitle("Dobbelstenen");
-        stage.setOnCloseRequest(e -> dice.close());
+        FXMLLoader fxmlLoader = new FXMLLoader(Ugentopoly.class.getResource("Ugentopoly.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Ugentopoly");
         stage.setScene(scene);
         stage.show();
-        //test
-    }
+        stage.setResizable(false);
 
+
+    }
     public static void main(String[] args) {
         launch();
     }
---
+++ src/main/java/be/ugent/objprog/ugentopoly/UgentopolyController.java
@@ -0,0 +1,34 @@
+package be.ugent.objprog.ugentopoly;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class UgentopolyController{
+
+    @FXML BorderPane bord;
+    @FXML AnchorPane top;
+    @FXML AnchorPane left;
+    @FXML AnchorPane center;
+    @FXML AnchorPane  right;
+    @FXML AnchorPane bottom;
+
+
+    /** Niet geHardCode AnchorPanes.
+     public void initialize(){
+        for (int i = 0; i < 9; i ++) {
+            AnchorPane anchorPane = new AnchorPane();
+            anchorPane.setPrefSize(150, 55);
+
+            Double pos = i* 55.0;
+
+            AnchorPane.setTopAnchor(anchorPane,  pos);
+            anchorPane.setStyle("-fx-background-color: GREEN");
+
+            right(moet ook voor left, top en bottom.).getChildren().add(anchorPane);
+        }
+    } */
+
+}
